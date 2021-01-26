40a0edcce70cc0adaad25ef2473ad50762050aa5
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-01-26 16:46:48 +0100

    Update tailwind config to handle brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        'routine-dark': '#292827',
+        'routine-black': '#444',
+        'routine-blue': '#4754DC',
+        'routine-gray-1': '#F6F3EE',
+        'routine-gray-3': '#BBB2A1',
+        'routine-gray-4': '#D3CDB8',
+      },
+    },
   },
   variants: {
     extend: {},
