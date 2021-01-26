f98bd0b28b88a25f73c478ff2c88b3993cc7b206
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-01-26 17:02:01 +0100

    Build the input

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="h-screen bg-routine-dark text-routine-black">
+    <main class="container">
+      <section class="max-w-md py-28">
+        <input
+          placeholder="What will you pick?"
+          class="w-full px-6 py-4 text-2xl focus:ring focus:ring-routine-gray-3 focus:outline-none bg-routine-gray-1 placeholder-routine-gray-3"
+          type="text"
+        />
+      </section>
+    </main>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
